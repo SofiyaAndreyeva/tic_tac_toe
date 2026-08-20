a7688118e098ed6e74d57dbe4490dd2ae138a6a2
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-08-20 21:02:29 +0300

    add app sizes

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,19 @@
+class AppSizes {
+  static double size16 = 16;
+  static double size50 = 50;
+  static double size80 = 80;
+  static double size20 = 20;
+  static double size30 = 30;
+  static double size35 = 35;
+  static double size10 = 10;
+  static double size3 = 3;
+  static double size2 = 2;
+  static double size1 = 1;
+  static double size8 = 8;
+  static double size5 = 5;
+  static double zero = 0;
+  static double size03 = 0.3;
+  static double size07 = 0.7;
+  static double size08 = 0.8;
+  static double size01 = 0.1;
+}
