ad05b1dd72da5062e98cf5f79cb9d98a48ec6a3f
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-08-20 21:23:28 +0300

    add history cubit

--- lib/core/bloc/app_bloc.dart
+++ lib/core/bloc/app_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tic_tac_toe/core/di/get_it.dart';
 import 'package:tic_tac_toe/feature/game/cubit/game_cubit.dart';
+import 'package:tic_tac_toe/feature/history/cubit/game_history_cubit.dart';
 
 class AppBloc {
   static final List<BlocProvider> appBlocs = [
     BlocProvider<GameCubit>(create: (_) => getIt<GameCubit>()),
+    BlocProvider<GameHistoryCubit>(create: (_) => getIt<GameHistoryCubit>()),
   ];
 }
